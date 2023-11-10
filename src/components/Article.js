717c10c056bcbf9a6130da566705e7f551c5d62c
clementmwamba <clementmwamba23@gmail.com>
2023-11-10 16:31:27 +0300

    final

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 function Article({title, date, preview, minutes}) {
+  
   return (
     <>
-        <h3>Title: {title}</h3>
+        <h3 style={{color: 'pink'}}>Title : {title}</h3>
         <small>Date: {date || 'January 1, 1970'}</small>
         <p>Read: {preview}</p>
         <p>{`${minutes} minutes`}</p>
